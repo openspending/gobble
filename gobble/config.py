6370058ffa36b0415dba64897526240e724c32b6
cyberbikepunk <loic.jounot@gmail.com>
2016-06-30 12:47:25 +0200

    Pasteurize config module and add HOST and PORT settings.

--- gobble/config.py
+++ gobble/config.py
@@ -1,3 +1,13 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+# noinspection PyCompatibility
+from builtins import open
+from future import standard_library
+standard_library.install_aliases()
+
 from json import loads
 from os.path import join
 from pprint import pprint
@@ -7,9 +17,12 @@ GOOGLE_FILEPATH = join(SECRETS_DIR, 'google.json')
 GOOGLE_OAUTH = loads(open(GOOGLE_FILEPATH).read())['installed']
 REDIRECT_URL = 'http://127.0.0.1:5000/oauth/check'
 GOOGLE_API_URL = 'https://accounts.google.com/o/oauth2/auth'
-
 SCOPE = ['https://www.googleapis.com/auth/userinfo.email',
          'https://www.googleapis.com/auth/userinfo.profile']
+
+
+HOST = '0.0.0.0'
+PORT = '5000'
 
 
 if __name__ == '__main__':
