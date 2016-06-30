3b9f743f6d7bca11ce4c35d84b891b2c0040f81d
cyberbikepunk <loic.jounot@gmail.com>
2016-06-30 09:49:16 +0200

    Start with a generic OAuth script.

---
+++ gobble/config.py
@@ -0,0 +1,16 @@
+from json import loads
+from os.path import join
+from pprint import pprint
+
+SECRETS_DIR = '/secrets'
+GOOGLE_FILEPATH = join(SECRETS_DIR, 'google.json')
+GOOGLE_OAUTH = loads(open(GOOGLE_FILEPATH).read())['installed']
+REDIRECT_URL = 'http://127.0.0.1:5000/oauth/check'
+GOOGLE_API_URL = 'https://accounts.google.com/o/oauth2/auth'
+
+SCOPE = ['https://www.googleapis.com/auth/userinfo.email',
+         'https://www.googleapis.com/auth/userinfo.profile']
+
+
+if __name__ == '__main__':
+    pprint({k: v for k, v in locals().items() if k == k.upper()})
---
+++ gobble/user.py
@@ -0,0 +1,29 @@
+"""Authenticate the user in Open-Spending."""
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+
+from requests_oauthlib import OAuth2Session
+from gobble.config import GOOGLE_OAUTH, REDIRECT_URL, SCOPE, GOOGLE_API_URL
+
+
+if __name__ == '__main__':
+    oauth = OAuth2Session(
+        GOOGLE_OAUTH['client_id'],
+        redirect_uri=REDIRECT_URL,
+        scope=SCOPE
+    )
+    # access_type and approval_prompt are Google specific extra parameters.
+    authorization_url, state = oauth.authorization_url(
+        GOOGLE_API_URL,
+        access_type='offline',
+        approval_prompt='force',
+    )
+
+    print ('Please go to %s and authorize access.' % authorization_url)
+    authorization_response = input('Enter the full callback URL')
+
