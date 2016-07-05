305a137352ee2a287ded5d39657f91460e474e3b
cyberbikepunk <loic.jounot@gmail.com>
2016-07-05 20:56:34 +0200

    Add tests for ElasticSearch classes.

---
+++ tests/test_search.py
@@ -0,0 +1,22 @@
+"""Test the ElasticSearch module"""
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+
+from pytest import raises
+from gobble.search import Contributors, BadSearchKey
+
+
+def test_validate_bad_query_raises_exception():
+    with raises(BadSearchKey):
+        Contributors().validate_query(foo='bar')
+
+
+def test_validate_good_query_adds_double_quotes():
+    original = {'name': 'mickey mouse'}
+    validated = {'name': '"mickey mouse"'}
+    assert Contributors().validate_query(**original) == validated
