4ef4342b5b5c3bd8399a6feaf065dbe9f4fe5235
cyberbikepunk <loic.jounot@gmail.com>
2016-07-17 23:05:00 +0200

    Add tests to the configuration module (forgotten previously).

---
+++ tests/test_configuration.py
@@ -0,0 +1,71 @@
+"""Test the configuration module"""
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+
+standard_library.install_aliases()
+
+from json import loads
+from os.path import isfile
+from shutil import rmtree
+from munch import Munch
+from pytest import fixture
+from io import open
+
+from gobble.configuration import Config
+
+
+@fixture(scope='module')
+def dummy_config(request):
+    dummy_defaults = {
+        'CONFIG_FILE': '/tmp/gobble-dummy/dummy.json'
+    }
+
+    def delete():
+        try:
+            rmtree('/tmp/gobble-dummy')
+        except OSError:
+            pass
+
+    request.addfinalizer(delete)
+    return Config(dummy_defaults).save()
+
+
+# noinspection PyShadowingNames
+def test_config_object_is_not_empty(dummy_config):
+        assert dummy_config
+
+
+# noinspection PyShadowingNames
+def test_config_object_is_correct_length(dummy_config):
+        assert len(dummy_config) == 1
+
+
+# noinspection PyShadowingNames
+def test_config_object_is_instance_of_munch_class(dummy_config):
+    assert isinstance(dummy_config, Munch)
+
+
+# noinspection PyShadowingNames
+def test_config_object_is_accessible_by_keyword(dummy_config):
+    assert 'CONFIG_FILE' in dummy_config
+
+
+# noinspection PyShadowingNames
+def test_setting_object_is_accessible_by_attribute(dummy_config):
+    assert dummy_config.CONFIG_FILE
+
+
+# noinspection PyShadowingNames
+def test_config_file_has_been_created(dummy_config):
+    assert isfile(dummy_config.CONFIG_FILE)
+
+
+# noinspection PyShadowingNames
+def test_loading_config_file_returns_original_data(dummy_config):
+    with open(dummy_config.CONFIG_FILE) as file:
+        config_dict = loads(file.read())
+        assert config_dict == {'CONFIG_FILE': '/tmp/gobble-dummy/dummy.json'}
