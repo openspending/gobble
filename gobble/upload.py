4ef16ba328f7a1dc21843f835f43e33db7fdc60c
cyberbikepunk <loic.jounot@gmail.com>
2016-08-02 21:54:09 +0200

    Turn streams and futures into class attributes and improve docstrings

--- gobble/upload.py
+++ gobble/upload.py
@@ -35,60 +35,80 @@ token = user.permissions['os.datastore']['token']
 owner_id = user.authentication['profile']['idhash']
 
 
-def upload_datapackage(filepath, publish=True):
-    """Upload a datapackage to the Open-Spending datastore.
+def push(target, publish=False):
+    """Upload a fiscal datapackage to Open-Spending.
+
+    The target is the full path to the fiscal datapackage JSON descriptor,
+    but it can also be a dictionary representing the schema itself or a url
+    pointing to a descriptor (for more information please refer to the
+    documentation for the :class:`datapackage.DataPackage` class.
+
+    By default, newly uploaded packages are kept private, but you can change
+    that. Also note that if you upload a datapackage twice, the first one will
+    be overwritten. For now, the only valid datafile format is CSV.
 
     :param publish: toggle the datapackage to "published" after upload
-
-    :param filepath: can be `dict`, `str` or file-like object (see the docs
-           for the `datapackage.DataPackage` class)
+    :param target: absolute path to package descriptor or url or schema
     """
-    batch = Batch(filepath)
+    batch = Batch(target)
 
     for target in batch.request_s3_urls():
-        promise = batch.push_to_s3(*target)
-        batch.handle_promise(*promise)
+        batch.push_to_s3(*target)
 
-    batch.insert_into_postgres()
+    batch.handle_promises()
+    batch.insert_into_datastore()
 
     while batch.in_progress:
         sleep(POLL_PERIOD)
 
-    log.info('Congratuations, %s was uploaded successfully!', batch)
-
     if publish:
-        toggle_public_access(batch.name)
+        toggle(batch.name)
 
 
-def toggle_public_access(package_name, public=True):
+class ToggleError(Exception):
+    pass
+
+
+def toggle(package_name, public=True):
+    """Toggle public access to a fiscal datapackage
+
+    Change the status of a fiscal data package from public to private or
+    vice-versa. If something went wrong, whilst changing the status, you will
+    get a :class:`upload.ToggleError`.
+
+    :param package_name: the unique name of the datapackage
+    :param public: whether the package should be public or private
+    :return: the new state of the package, i.e. "public" or "private"
+    """
     to_state = 'public' if public else 'private'
 
-    toggle = str(public).lower()
+    publish = str(public).lower()
     package_id = owner_id + ':' + package_name
-    query = dict(jwt=token, id=package_id, publish=toggle)
+    query = dict(jwt=token, id=package_id, publish=publish)
+
     answer = handle(toggle_publish(params=query))
 
     if not answer['success']:
-        raise ValueError('Unable to toggle datapackage to %s', to_state)
+        message = 'Unable to toggle datapackage to %s'
+        raise ToggleError(message, to_state)
 
     log.info('%s is now %s', package_id, to_state)
     return to_state
 
 
-def check_datapackage_schema(filepath, raise_error=True):
-    """Validate a datapackage.
+def validate(target, raise_error=True, schema='fiscal'):
+    """Validate a datapackage schema.
 
-    :param filepath: can be `dict`, `str` or file-like object (see the docs
-           for the `datapackage.DataPackage` class)
-
-    :param raise_error: a 'bool' flag
+    :param target: A valid datapackage target (`datapackage.DataPackage`).
+    :param raise_error: raise a `datapackage.Validation` error if invalid
+    :param schema: the schema to validate against:
 
     :return By default, return true if the package is valid, else return
             a list of error messages. If the `raise_error` flag is True,
             however, raise a `datapackage.exceptions.ValidatioError`.
 
     """
-    package = DataPackage(filepath)
+    package = DataPackage(target, schema=schema)
     name = package.descriptor.get('name') or 'datapackage'
 
     try:
@@ -132,23 +152,36 @@ def compute_hash(filepath):
 
 
 class Batch(DataPackage):
-    """This class uploads a datdpackage to the datastore."""
+    """This class uploads a datapackage to Open-Spending.
 
-    def __init__(self, filepath):
-        super(Batch, self).__init__(filepath, schema='fiscal')
+    It does this in 3 steps:
+        * request urls for AWS S3 storage
+        * upload all files to the owner's S3 bucket
+        * insert the data into the Open-Spending datastore (PostgreSQL)
+
+    This class is a subclass of the :class:`datapackage.DataPackage` class.
+    The constructor takes the same arguments as its parent class, except that
+    the default schema is "fiscal".
+    """
+
+    def __init__(self, filepath, schema='fiscal', **kwargs):
+        super(Batch, self).__init__(filepath, schema=schema, **kwargs)
 
         self.validate()
-        self.check_file_formats()
+        self._check_file_formats()
 
-        self.responses = []
+        self.streams = []
         self._session = FuturesSession()
+        self.futures = []
+        self.responses = []
+
         self.name = self.descriptor['name']
         self.path = basename(filepath)
         self.filepath = filepath
 
         log.info('Starting uploading process for %s', self)
 
-    def check_file_formats(self):
+    def _check_file_formats(self):
         for resource in self:
             if resource.descriptor['mediatype'] != 'text/csv':
                 message = 'Usupported format: %s, valid formats are %s'
@@ -181,37 +214,35 @@ class Batch(DataPackage):
             }
         }
 
-    def get_header(self, path):
-        """The request header for the upload.
-        """
+    def _get_header(self, path):
         filepath = join(self.base_path, path)
-        return {
-            'Content-Length': getsize(filepath),
-            'Content-MD5': compute_hash(filepath)
-        }
+        return {'Content-Length': getsize(filepath),
+                'Content-MD5': compute_hash(filepath)}
 
     @property
-    def package_url(self):
+    def _descriptor_s3_url(self):
         return join(settings.S3_BUCKET_URL, owner_id, self.name, self.path)
 
     def request_s3_urls(self):
-        """S3 urls for uploading datafiles"""
+        """Request AWS S3 upload urls for all files.
+
+        :return : a generator of tuples: (url, realtive path, query, header)
+                  for the method `push_to_s3` to iterate on.
+        """
 
         response = request_upload(params=dict(jwt=token), json=self.files)
         files = handle(response)['filedata']
 
         for path, info in files.items():
-            query = {k: v[0] for k, v in info['upload_query'].items()}
             message = '%s is ready for upload to %s'
             log.info(message, path, info['upload_url'])
-
-            yield info['upload_url'], path, query, self.get_header(path)
+            query = {k: v[0] for k, v in info['upload_query'].items()}
+            yield info['upload_url'], path, query, self._get_header(path)
 
     def push_to_s3(self, url, path, headers, query):
         """Send data files for upload to the S3 bucket.
         """
         log.debug('Started uploading %s to %s', path, url)
-
         absolute_path = join(self.base_path, path)
         stream = io.open(absolute_path, mode='rb')
         future = self._session.put(url,
@@ -220,47 +251,57 @@ class Batch(DataPackage):
                                    params=query,
                                    background_callback=self._s3_callback)
 
-        return [future, stream]
+        self.streams.append(stream)
+        self.futures.append(future)
 
     @staticmethod
     def _s3_callback(_, response):
-        """Report a succesfully S3 upload or fail.
-        """
         handle(response)
         log.info('Successful S3 upload: %s', response.url)
 
-    def handle_promise(self, future, stream):
-        """Collect a promise from S3 uploads
+    def handle_promises(self):
+        """Collect all promises from S3 uploads.
         """
-        exception = future.exception()
-        if exception:
-            raise exception
-        response = future.result()
+        for stream, future in zip(self.streams, self.futures):
+            exception = future.exception()
+            if exception:
+                raise exception
+            response = future.result()
 
-        if response.status_code != 200:
-            message = 'Something went wrong uploading %s to S3: %s'
-            log.error(message, response.url, response.text)
-            raise HTTPError(message)
+            if response.status_code != 200:
+                message = 'Something went wrong uploading %s to S3: %s'
+                log.error(message, response.url, response.text)
+                raise HTTPError(message)
 
-        self.responses.append(response)
-        stream.close()
+            self.responses.append(response)
+            stream.close()
 
-    def insert_into_postgres(self):
+    def insert_into_datastore(self):
         """Transfer datafiles from S3 into the postgres datastore.
+
+        :return: the url of the fiscal datapackage on Open-Spending
         """
-        query = dict(jwt=token, datapackage=self.package_url)
+        query = dict(jwt=token, datapackage=self._descriptor_s3_url)
         response = upload_package(params=query)
-        return handle(response)
+        handle(response)
+
+        log.info('Congratuations, %s was uploaded successfully!', self)
+        log.info('You can find you fiscal datapackage here: %s', self.os_url)
+
+        return self.os_url
+
+    @property
+    def os_url(self):
+        return join(settings.OS_URL, owner_id + ':' + self.name)
 
     @property
     def in_progress(self):
-        """Return true when the upload status is 'done'.
-        """
-        query = dict(datapackage=self.package_url)
+        """Return true when the upload finished."""
+
+        query = dict(datapackage=self._descriptor_s3_url)
         answer = upload_status(params=query).json()
         args = self, answer['status'], answer['progress'], len(self)
         log.debug('%s is loading (%s) %s/%s', *args)
-
         return answer['status'] != 'done'
 
     def __len__(self):
